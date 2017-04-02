370933816758d25aea8117699e1e5690ab761ea2
Daiki Mizukami <d12i@users.noreply.github.com>
2017-04-03 05:03:56 +0900

    Update tweetust to v0.6

--- src/auth.rs
+++ src/auth.rs
@@ -58,16 +58,20 @@ impl<'a> From<Token<'a>> for egg_mode::Token<'a> {
 
 #[cfg(feature = "tweetust")]
 impl<'a> tweetust::conn::Authenticator for Token<'a> {
-    type Scheme = OAuthAuthorizationHeader;
+    type Scheme = tweetust::conn::oauth_authenticator::OAuthAuthorizationScheme;
 
-    fn create_authorization_header(&self, request: &tweetust::conn::Request) -> Option<OAuthAuthorizationHeader> {
+    fn create_authorization_header(&self, request: &tweetust::conn::Request)
+        -> Option<tweetust::conn::oauth_authenticator::OAuthAuthorizationScheme>
+    {
         let params = if let tweetust::conn::RequestContent::WwwForm(ref params) = request.content {
             Some(params.as_ref().iter().map(|&(ref k, ref v)| (k.as_ref(), v.as_ref())))
         } else {
             None
         };
 
-        Some(authorize(self, request.method.as_ref(), &request.url, params))
+        Some(tweetust::conn::oauth_authenticator::OAuthAuthorizationScheme(
+            authorize(self, request.method.as_ref(), &request.url, params)
+        ))
     }
 }
 
