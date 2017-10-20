3bc9e1464ba349a7c1e41cdb84eff6cfab02e1a0
Ryan Leckey <ryan@launchbadge.com>
2017-10-20 16:52:09 -0700

    uncomment (and upgrade) hyper-rustls

--- twitter-stream/src/lib.rs
+++ twitter-stream/src/lib.rs
@@ -602,7 +602,7 @@ mod default_connector {
         HttpsConnector::new(1, h)
     }
 }
-/*
+
 #[cfg(all(feature = "tls-rustls", not(feature = "tls")))]
 mod default_connector {
     extern crate hyper_rustls;
@@ -615,7 +615,7 @@ mod default_connector {
         Ok(HttpsConnector::new(1, h))
     }
 }
-*/
+
 #[cfg(all(feature = "tls-openssl", not(any(feature = "tls", feature = "tls-rustls"))))]
 mod default_connector {
     extern crate hyper_openssl;
