2436ad022dc253a70a3eba4d2212bf0a4f27a646
Daiki Mizukami <tesaguriguma@gmail.com>
2019-05-11 03:15:13 +0900

    Update example

--- examples/echo_bot.rs
+++ examples/echo_bot.rs
@@ -1,13 +1,47 @@
+extern crate serde;
 extern crate serde_json as json;
 extern crate tweetust;
 extern crate twitter_stream;
-extern crate twitter_stream_message;
 
 use std::fs::File;
 use std::path::PathBuf;
+
+use serde::de;
+use serde::Deserialize;
 use twitter_stream::rt::{self, Future, Stream};
 use twitter_stream::{Error, Token, TwitterStreamBuilder};
-use twitter_stream_message::StreamMessage;
+
+#[derive(Deserialize)]
+#[serde(untagged)]
+enum StreamMessage {
+    Tweet(Tweet),
+    Other(de::IgnoredAny),
+}
+
+#[derive(Deserialize)]
+struct Tweet {
+    created_at: String,
+    entities: Entities,
+    id: i64,
+    text: String,
+    user: User,
+}
+
+#[derive(Deserialize)]
+struct Entities {
+    user_mentions: Vec<UserMention>,
+}
+
+#[derive(Deserialize)]
+struct UserMention {
+    id: i64,
+}
+
+#[derive(Deserialize)]
+struct User {
+    id: i64,
+    screen_name: String,
+}
 
 fn main() {
     const TRACK: &str = "@NAME_OF_YOUR_ACCOUNT";
@@ -22,9 +56,10 @@ fn main() {
     let credential = File::open(credential_path).unwrap();
     let token: Token = json::from_reader(credential).unwrap();
 
-    let stream = TwitterStreamBuilder::filter(&token)
+    let stream = TwitterStreamBuilder::filter(token.borrowed())
         .track(Some(TRACK))
         .listen()
+        .unwrap()
         .flatten_stream();
     let rest = tweetust::TwitterClient::new(
         token,
@@ -41,13 +76,13 @@ fn main() {
 
     let bot = stream
         .for_each(move |json| {
-            if let Ok(StreamMessage::Tweet(tweet)) = StreamMessage::from_str(&json) {
-                if tweet.user.id != user.id as u64
+            if let Ok(StreamMessage::Tweet(tweet)) = json::from_str(&json) {
+                if tweet.user.id != user.id
                     && tweet
                         .entities
                         .user_mentions
                         .iter()
-                        .any(|mention| mention.id == user.id as u64)
+                        .any(|mention| mention.id == user.id)
                 {
                     println!(
                         "On {}, @{} tweeted: {:?}",
@@ -57,7 +92,7 @@ fn main() {
                     let response = format!("@{} {}", tweet.user.screen_name, tweet.text);
                     rest.statuses()
                         .update(response)
-                        .in_reply_to_status_id(tweet.id as _)
+                        .in_reply_to_status_id(tweet.id)
                         .execute()
                         .map_err(Error::custom)?;
                 }
