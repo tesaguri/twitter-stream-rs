a8d02a17713ed714b3166415cd0200b98c79b590
Daiki Mizukami <tesaguriguma@gmail.com>
2019-12-22 07:48:47 +0900

    Bump to v0.10.0-alpha.4

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/twitter-stream/0.10.0-alpha.3")]
+#![doc(html_root_url = "https://docs.rs/twitter-stream/0.10.0-alpha.4")]
 
 /*!
 # Twitter Stream
@@ -13,7 +13,7 @@ Add `twitter-stream` to your dependencies in your project's `Cargo.toml`:
 [dependencies]
 futures = "0.3"
 tokio = { version = "0.2", features = ["macros"] }
-twitter-stream = "=0.10.0-alpha.3"
+twitter-stream = "=0.10.0-alpha.4"
 ```
 
 ## Overview
